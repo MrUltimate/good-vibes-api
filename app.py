c84817dd4ad70f7947bf9c8447a744acfbaade10
Shivam Sinha <sinha.shivam199@gmail.com>
2020-07-09 19:12:59 -0400

    Fix Port

--- app.py
+++ app.py
@@ -136,4 +136,4 @@ async def get_reddit_urls():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ['PORT']))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', '5000')))
