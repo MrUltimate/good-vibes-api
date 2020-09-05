ebadf3dab9f13f1504209fd3379070b8f588d45a
Shivam Sinha <sinha.shivam199@gmail.com>
2020-09-04 20:13:33 -0400

    Update Scraper

--- scrape.py
+++ scrape.py
@@ -37,7 +37,7 @@ async def get_reddit_urls():
 
 async def main(urls):
     tasks = []
-    async with aiohttp.ClientSession() as session:
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False, use_dns_cache=False,), headers={"Connection": "close"}) as session:
         for url in urls:
             task = asyncio.ensure_future(get_article(session, url))
             tasks.append(task)
@@ -50,85 +50,87 @@ async def get_article(session, url):
 
     print(f'Processing URL: {url}')
 
-    async with session.get(url, timeout=ClientTimeout(total=0)) as response:
-        content = await response.read()
-        article = newspaper.Article(url)
-        article.set_html(content)
-        article_dict = {}
-        article_dict['last_updated'] = datetime.now().strftime(
-            "%d/%m/%Y %H:%M:%S")
-
-        if response.status == 200:
-            article.parse()
-            article_dict['status'] = 'ok'
-
-            article_dict['article'] = {}
-            article_dict['article']['source_url'] = article.source_url
-
-            try:
-                guess = guess_date(url=url, html=article.html)
-                article_dict['article']['published'] = guess.date
-                article_dict['article']['published_method_found'] = guess.method
-                article_dict['article']['published_guess_accuracy'] = None
-                if guess.accuracy is Accuracy.PARTIAL:
-                    article_dict['article']['published_guess_accuracy'] = 'partial'
-                if guess.accuracy is Accuracy.DATE:
-                    article_dict['article']['published_guess_accuracy'] = 'date'
-                if guess.accuracy is Accuracy.DATETIME:
-                    article_dict['article']['published_guess_accuracy'] = 'datetime'
-                if guess.accuracy is Accuracy.NONE:
-                    article_dict['article']['published_guess_accuracy'] = None
-            except:
-                article_dict['article']['published'] = article.publish_date
-                article_dict['article']['published_method_found'] = None
-                article_dict['article']['published_guess_accuracy'] = None
-
-            article.nlp()
-
-            article_dict['article']['title'] = article.title
-            article_dict['article']['summary'] = article.summary
-            article_dict['article']['keywords'] = list(article.keywords)
-            article_dict['article']['authors'] = list(article.authors)
-
-            sentiString = article.title + article.summary
-            analyzer = SentimentIntensityAnalyzer()
-            vs = analyzer.polarity_scores(sentiString)
-
-            try:
-                title_lang = detect(article.title)
-            except:
-                title_lang = None
-
-            try:
-                text_lang = detect(article.text)
-            except:
-                text_lang = None
-
-            article_dict['article']['images'] = list(article.images)
-            article_dict['article']['top_image'] = article.top_image
-            article_dict['article']['meta_image'] = article.meta_img
-            article_dict['article']['movies'] = list(article.movies)
-            article_dict['article']['meta_keywords'] = list(
-                article.meta_keywords)
-            article_dict['article']['tags'] = list(article.tags)
-            article_dict['article']['meta_description'] = article.meta_description
-            article_dict['article']['meta_lang'] = article.meta_lang
-            article_dict['article']['title_lang'] = str(title_lang)
-            article_dict['article']['text_lang'] = str(text_lang)
-            article_dict['article']['meta_favicon'] = article.meta_favicon
-            article_dict['article']['url'] = str(url)
-            article_dict['article']['positive'] = vs['pos']
-            article_dict['article']['neutral'] = vs['neu']
-            article_dict['article']['negative'] = vs['neg']
-            finalJSON.append(article_dict)
-            # return article_dict
-            # finalJSON = {**finalJSON, **article_dict}
-        else:
+    sem = asyncio.Semaphore(10)
+    async with sem:
+        async with session.get(url, timeout=ClientTimeout(total=0)) as response:
+            content = await response.read()
+            article = newspaper.Article(url)
+            article.set_html(content)
             article_dict = {}
-            article_dict['status'] = 'error'
-            article_dict['article'] = 'An error occured parsing this article'
-            # return article_dict
-            finalJSON.append(article_dict)
+            article_dict['last_updated'] = datetime.now().strftime(
+                "%d/%m/%Y %H:%M:%S")
+
+            if response.status == 200:
+                article.parse()
+                article_dict['status'] = 'ok'
+
+                article_dict['article'] = {}
+                article_dict['article']['source_url'] = article.source_url
+
+                try:
+                    guess = guess_date(url=url, html=article.html)
+                    article_dict['article']['published'] = guess.date
+                    article_dict['article']['published_method_found'] = guess.method
+                    article_dict['article']['published_guess_accuracy'] = None
+                    if guess.accuracy is Accuracy.PARTIAL:
+                        article_dict['article']['published_guess_accuracy'] = 'partial'
+                    if guess.accuracy is Accuracy.DATE:
+                        article_dict['article']['published_guess_accuracy'] = 'date'
+                    if guess.accuracy is Accuracy.DATETIME:
+                        article_dict['article']['published_guess_accuracy'] = 'datetime'
+                    if guess.accuracy is Accuracy.NONE:
+                        article_dict['article']['published_guess_accuracy'] = None
+                except:
+                    article_dict['article']['published'] = article.publish_date
+                    article_dict['article']['published_method_found'] = None
+                    article_dict['article']['published_guess_accuracy'] = None
+
+                article.nlp()
+
+                article_dict['article']['title'] = article.title
+                article_dict['article']['summary'] = article.summary
+                article_dict['article']['keywords'] = list(article.keywords)
+                article_dict['article']['authors'] = list(article.authors)
+
+                sentiString = article.title + article.summary
+                analyzer = SentimentIntensityAnalyzer()
+                vs = analyzer.polarity_scores(sentiString)
+
+                try:
+                    title_lang = detect(article.title)
+                except:
+                    title_lang = None
+
+                try:
+                    text_lang = detect(article.text)
+                except:
+                    text_lang = None
+
+                article_dict['article']['images'] = list(article.images)
+                article_dict['article']['top_image'] = article.top_image
+                article_dict['article']['meta_image'] = article.meta_img
+                article_dict['article']['movies'] = list(article.movies)
+                article_dict['article']['meta_keywords'] = list(
+                    article.meta_keywords)
+                article_dict['article']['tags'] = list(article.tags)
+                article_dict['article']['meta_description'] = article.meta_description
+                article_dict['article']['meta_lang'] = article.meta_lang
+                article_dict['article']['title_lang'] = str(title_lang)
+                article_dict['article']['text_lang'] = str(text_lang)
+                article_dict['article']['meta_favicon'] = article.meta_favicon
+                article_dict['article']['url'] = str(url)
+                article_dict['article']['positive'] = vs['pos']
+                article_dict['article']['neutral'] = vs['neu']
+                article_dict['article']['negative'] = vs['neg']
+                finalJSON.append(article_dict)
+                # return article_dict
+                # finalJSON = {**finalJSON, **article_dict}
+            else:
+                article_dict = {}
+                article_dict['status'] = 'error'
+                article_dict['article'] = 'An error occured parsing this article'
+                # return article_dict
+                finalJSON.append(article_dict)
 
 if __name__ == "__main__":
     asyncio.run(get_reddit_urls())
